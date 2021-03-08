a4c71d1e8cd97bf4233a0a5f82c6d6652fee1d93
Cristian Vargas <cvg97@misena.edu.co>
2021-03-07 23:22:53 -0500

    implement of update windows with fps and fps

--- src/game/Game.java
+++ src/game/Game.java
@@ -14,6 +14,8 @@ public class Game extends Canvas implements Runnable{
 	private static JFrame window;
 	private static Thread thread;
 	private volatile boolean  running = true;
+	private static int aps = 0;
+	private static int fps = 0;
 	
 	private Game() {
 		setPreferredSize(new Dimension(WIDTH, HEIGH));
@@ -48,12 +50,45 @@ public class Game extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-
+	
+	private void updateGame() {
+		aps++;
+	}
+	
+	private void showGame() {
+		fps++;
+	}
+	
 	@Override
 	public void run() {
+		final int NS_BY_SECONDS = 1000000000;
+		final byte APS_OBJECTIVE = 60;
+		final double NS_BY_UPDATE = NS_BY_SECONDS / APS_OBJECTIVE;
+		
+		long refenceUpdate = System.nanoTime();
+		long refCount = System.nanoTime();
+		double timeElapsed;
+		double delta = 0;
+		
 		while(running) {
+			final long initLoop = System.nanoTime();
+			timeElapsed =  initLoop - refenceUpdate;
+			refenceUpdate = initLoop;
+			delta+= timeElapsed / NS_BY_UPDATE;
 			
+			while(delta >= 1) {
+				updateGame();
+				delta-=1;
+			}
+			showGame();
+			if(System.nanoTime() - refCount > NS_BY_SECONDS) {
+				window.setTitle(NAME + " APS: " + aps + " FPS:" + fps);
+				aps = 0;
+				fps =0;
+				refCount = System.nanoTime();
+			}
 		}
 	}
 
 }
+
