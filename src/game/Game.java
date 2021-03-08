392e312dfc7e7ebd7664e4c65b5d42886ff4c731
Cristian Vargas <cvg97@misena.edu.co>
2021-03-07 22:32:03 -0500

    support for threads

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,12 +6,14 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas{
+public class Game extends Canvas implements Runnable{
 	public static final long serialVersinUID = 1L;
 	public static final int WIDTH = 800;
 	public static final int HEIGH = 600;
 	public static final String NAME="ZombiLand";
 	private static JFrame window;
+	private static Thread thread;
+	private volatile boolean  running = true;
 	
 	private Game() {
 		setPreferredSize(new Dimension(WIDTH, HEIGH));
@@ -28,6 +30,30 @@ public class Game extends Canvas{
 	
 	public static void main(String[] args) {
 		 Game game = new Game();
+		 game.start();
+		 
+	}
+
+	public synchronized void start() {
+		 this.running = true;
+		 thread =  new Thread(this, "graphics");
+		 thread.start();
+	}
+	
+	public synchronized void stop() {
+		this.running= false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while(running) {
+			
+		}
 	}
 
 }
